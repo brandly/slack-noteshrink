f66b4cddfdb9325e192555444d441aca862ff4a9
brandly <brandly13@gmail.com>
2018-03-19 11:54:09 -0400

    block files from getting handled twice

--- server.py
+++ server.py
@@ -34,18 +34,25 @@ def index():
 
 # TODO: properly respond so we don't get retried requests
 # API wants a 200 within 3 seconds
+handled_files = []
 def handle_file_id(file_id, cmd=''):
+    if file_id in handled_files:
+        # this is a hack because i'm unsure how to respond quickly before
+        # shrinking and uploading the image.
+        return 'already taken care of'
+
+    handled_files.append(file_id)
     response = get_file_info(file_id)
 
     if response['ok']:
         file = response['file']
 
         if file['filetype'] not in ['jpg', 'jpeg', 'png']:
-            return
+            return 'not an image'
 
         title = file['title'].lower() + cmd
         if 'whiteboard' not in title and 'shrink' not in title:
-            return
+            return 'no shrink needed'
 
         return handle_img_file(file)
 
